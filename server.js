98377c260d5979118559a2df3d38cd2a3d007711
Tony Edwards <tony@plymouthsoftware.com>
2017-10-16 23:42:46 +0100

    Updates port
    Allows the app to run on heroku

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ var app = express();
 
 app.use(express.static(__dirname + '/dist/'));
 
-app.listen(9000, function () {
+app.listen(process.env.PORT || 9000, function () {
   console.log('Team Dashboard is serving directory ' + __dirname + ' on port 9000');
 });
